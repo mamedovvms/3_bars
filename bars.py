f4d84e392b073909e6372bd608c53971fd004772
1237 <mamedov_vs@oksw.ru>
2019-04-26 19:46:12 +0700

    Получение параметров переделано. Измены имена переменных. Добавлена функция получения данных для вывода на печать. Добавлена проверка существования файла и того, что данные в нем находятся в формате JSON

--- bars.py
+++ bars.py
@@ -1,40 +1,67 @@
 import json
+import argparse
+import os
 
 
 def load_data(filepath):
     with open(filepath) as f:
-        return json.loads(f.read())
+        try:
+            return json.loads(f.read())['features']
+        except json.JSONDecodeError:
+            return None
 
 
-def get_biggest_bar(json_data):
-    return max(json_data['features'],
-               key=lambda item: item['properties']['Attributes']['SeatsCount'])
+def get_biggest_bar(list_data_bars):
+    return max(list_data_bars,
+               key=lambda item: item['properties']['Attributes']['SeatsCount']
+               )
+
+def get_smallest_bar(list_data_bars):
+    return min(list_data_bars,
+               key=lambda item: item['properties']['Attributes']['SeatsCount']
+               )
 
 
-def get_smallest_bar(json_data):
-    return min(json_data['features'],
-               key=lambda item: item['properties']['Attributes']['SeatsCount'])
+def get_closest_bar(list_data_bars, longitude, latitude):
+    return min(list_data_bars,
+               key=lambda item: (item['geometry']['coordinates'][0] - longitude) ** 2 +
+               (item['geometry']['coordinates'][1] - latitude) ** 2
+               )
+
+def get_name_adress_bar(data_bar):
+    bar_attributes = data_bar['properties']['Attributes']
+    return bar_attributes['Name'], bar_attributes['Address']
+
+def main():
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('pathfile', help='File with data about bars in json format')
+    parser.add_argument('longitude', type=float, help='Longitude of current user gps coordinates')
+    parser.add_argument('latitude', type=float, help='Latitude of current user gps coordinates')
+
+    params = parser.parse_args()
+
+    filepath = params.pathfile
+    longitude = params.longitude
+    latitude = params.latitude
+
+    if not os.path.isfile(filepath):
+        exit('File not found')
 
 
-def get_closest_bar(json_data, longitude, latitude):
-    return min(json_data['features'],
-               key=lambda item: (
-               item['geometry']['coordinates'][0] - longitude) ** 2 +
-               (item['geometry']['coordinates'][1] - latitude) ** 2)
+    list_data_bars = load_data(filepath)
+
+    if not list_data_bars:
+        exit('File data is not in json format.')
+
+    biggest_bar = get_biggest_bar(list_data_bars)
+    smallest_bar = get_smallest_bar(list_data_bars)
+    closest_bar = get_closest_bar(list_data_bars, longitude, latitude)
+
+    print('Biggest bar:  {} по адресу {}'.format(*get_name_adress_bar(biggest_bar)))
+    print('Smallest bar: {} по адресу {}'.format(*get_name_adress_bar(smallest_bar)))
+    print('Closest bar:  {} по адресу {}'.format(*get_name_adress_bar(closest_bar)))
 
 
 if __name__ == '__main__':
-
-    filepath = input('Path to file: ')
-    longitude = float(input('Longitude: '))
-    latitude = float(input('Latitude: '))
-
-    json_data = load_data(filepath)
-
-    biggest_bar = get_biggest_bar(json_data)
-    smallest_bar = get_smallest_bar(json_data)
-    closest_bar = get_closest_bar(json_data, longitude, latitude)
-
-    print('Biggest bar:  ', biggest_bar)
-    print('Smallest bar: ', smallest_bar)
-    print('Closest bar:  ', closest_bar)
+    main()
